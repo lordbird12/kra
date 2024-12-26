31f97d90e967f5cc6c09104ab21451da68be20e1
erafan4054 <irafan4054@gmail.com>
2024-12-26 14:13:41 +0700

    pang

--- src/app/mock-api/common/navigation/data.ts
+++ src/app/mock-api/common/navigation/data.ts
@@ -352,15 +352,9 @@ export const defaultNavigation: FuseNavigationItem[] = [
         icon: 'heroicons_outline:home',
         children: [
 
-            // {
-            //     id: 'products.brand',
-            //     title: 'ยี่ห้อรถ',
-            //     type: 'basic',
-            //     icon: 'heroicons_outline:building-office-2',
-            //     link: '/admin/brand/list',
-            // },
+            
             {
-                id: 'products.category-product',
+                id: 'products.category-item',
                 title: 'ประเภทรายการ',
                 type: 'basic',
                 icon: 'heroicons_outline:cube',
@@ -374,7 +368,7 @@ export const defaultNavigation: FuseNavigationItem[] = [
                 link: '/admin/product/list',
             },
             {
-                id: 'category-attribute.list',
+                id: 'category-attribute.game',
                 title: 'ประเภท Game',
                 type: 'basic',
                 icon: 'heroicons_outline:wrench-screwdriver',
